d729cdb468bdfd1da16c37b161ee36ab44595698
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2023-05-14 10:25:44 +0200

    Add enum for player identifiers.

--- tic-tac-toe/src/main.rs
+++ tic-tac-toe/src/main.rs
@@ -1,3 +1,10 @@
+/// Symbols identifying the two different players in the game.
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Player {
+    X,
+    O,
+}
+
 fn main() {
     println!("Hello, world!");
 }
